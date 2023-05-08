beb8b326a5af4c4187ad835a5d5259a0b0f26731
eapepe0 <eapepe0@gmail.com>
2023-05-08 19:17:27 -0300

    Leccion 224 , Colocar el tipo de dato y almacenarlo en el state

--- src/context/places/placesReducer.ts
+++ src/context/places/placesReducer.ts
@@ -1,9 +1,13 @@
+import { Feature } from '../../interfaces/places'
 import { PlacesState } from './PlacesProvider'
 
-type PlacesAction = {
-    type : 'setUserLocation' , payload : [number , number],
+type PlacesAction = 
 
-}
+ |  {   type : 'setUserLocation' , payload : [number , number]     }
+ |  {   type : 'setPlaces' , payload : Feature []     }
+ |  {   type : 'setLoadingPlaces' }
+
+
 
 export const placesReducer = (state : PlacesState , action : PlacesAction) : PlacesState => {
   switch (action.type){
@@ -13,7 +17,18 @@ export const placesReducer = (state : PlacesState , action : PlacesAction) : Pla
             isLoading : false,
             userLocation : action.payload
         }
-
+    case 'setLoadingPlaces':
+        return {
+            ...state ,
+            isLoadingPlaces : true,
+            places : [],
+        }
+    case 'setPlaces':
+        return{
+            ...state,
+            isLoadingPlaces : false,
+            places : action.payload
+        }
     default:
         return state;
   }

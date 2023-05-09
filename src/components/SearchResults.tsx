232c4cac48a2a7d90e09ed0044b9e7661cc727a6
eapepe0 <eapepe0@gmail.com>
2023-05-08 22:22:33 -0300

    Leccion 227 , Volar y activar el lugar seleccionado

--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -1,17 +1,22 @@
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { MapContext, PlacesContext } from "../context"
+import { Feature } from "../interfaces/places"
 import { IsLoadingPlaces } from "./IsLoadingPlaces"
 
 export const SearchResults = () => {
     const { map , isMapReady } = useContext ( MapContext )
     const { places,isLoadingPlaces } = useContext(PlacesContext)
 
-/*     const onClick = (coords ) =>{
+    const [activeId , setActiveId] = useState('');
+
+    const onPlaceClicked = (place : Feature ) =>{
+        setActiveId(place.id)
+        const [ lng ,lat ] = place.center;
         map?.flyTo({
             zoom: 14,
-            center : coords
+            center : [lng , lat ]
         })
-    } */
+    }
 
     if(isLoadingPlaces){
         return (
@@ -24,12 +29,12 @@ export const SearchResults = () => {
     <ul className={`list-group ${places.length === 0  ?  ""  :  'mt-3' }`}>
         {
             places.map(place => (
-        <li className="list-group-item list-group-item-action" key={place.id}>
+        <li className={`list-group-item list-group-item-action pointer ${activeId === place.id  ?  'active'  :  "" }`} key={place.id} >
             <h6>{place.text}</h6>
-            <p className="text-muted" style={{ fontSize : "12px"}}>
+            <p className={`${activeId === place.id  ?  'text-selected text-reset'  :  "" } text-muted `} style={{fontSize: "12px"}}>
                 {place.place_name}
             </p>
-            <button className='btn btn-outline-primary btn-sm' /* onClick= {() => onClick(place.center)} */>
+            <button className={`btn  btn-sm ${activeId === place.id  ?  'btn-outline-light'  :  "btn-outline-primary" }`} onClick= {() => onPlaceClicked(place)}>
                 Direcciones
             </button>
         </li>
